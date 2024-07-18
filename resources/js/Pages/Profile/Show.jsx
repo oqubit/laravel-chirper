8ada0a0dde6b5ce5d39e640912556ab8ce96f920
o_Qubit <oqubit@users.noreply.github.com>
2024-07-18 11:03:08 +0300

    Add missing onChirpEdit/Delete functions to the public profile page

--- resources/js/Pages/Profile/Show.jsx
+++ resources/js/Pages/Profile/Show.jsx
@@ -1,20 +1,38 @@
 import Chirp from "@/Components/Chirp";
 import Heading from "@/Components/Heading";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
-import { Head, usePage } from "@inertiajs/react";
+import { Head, router, usePage } from "@inertiajs/react";
+import { useState } from "react";
 
 export default function Show({ user, chirps, following }) {
     const { auth } = usePage().props;
+    const [chirpz, setChirpz] = useState(chirps);
+
+    const onChirpEdit = (page) => {
+        setChirpz(page.props.chirps);
+    };
+
+    const onChirpDelete = (e, chirpID) => {
+        e.preventDefault();
+        router.delete(route("chirps.destroy", chirpID), {
+            onSuccess: (page) => {
+                setChirpz(page.props.chirps);
+            }
+        });
+    };
+
     return (
         <Authenticated user={auth.user}>
             <Head title={user.name} />
             <Heading user={user} following={following} />
             <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
                 <div className="mt-6 bg-white shadow-lg shadow-cyan-600 rounded-xl divide-y-4">
-                    {chirps.map((chirp) => (
+                    {chirpz.map((chirp) => (
                         <Chirp
                             key={chirp.id}
                             chirp={chirp}
+                            onChirpEdit={onChirpEdit}
+                            onChirpDelete={onChirpDelete}
                         />
                     ))}
                 </div>
